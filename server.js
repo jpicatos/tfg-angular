faa97bdae229e1a3be6e0becd886aea3ff06369a
jpicatos <jpicatos@ucm.es>
2018-11-10 12:42:30 +0100

    heroku3

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
-//Install express server
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
-let server = require('http').Server(app);
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/tfg-angular'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/tfg-angular'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/tfg-angular/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/tfg-angular/index.html'));
 });
 
-const port = process.env.PORT || 8000;
-// Start the app by listening on the default Heroku port
-server.listen(port, () => {
-    console.log("App is running on port " + port);
-});
+// default Heroku port
+app.listen(process.env.PORT || 5000);
