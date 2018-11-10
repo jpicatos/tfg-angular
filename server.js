7ad9b0d54caf136c00532f4b5b7dfeea092d9124
jpicatos <jpicatos@ucm.es>
2018-11-10 12:33:37 +0100

    heroku2

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+let server = require('http').Server(app);
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/tfg-angular'));
@@ -12,5 +13,8 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname,'/dist/tfg-angular/index.html'));
 });
 
+const port = process.env.PORT || 8000;
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+server.listen(port, () => {
+    console.log("App is running on port " + port);
+});
