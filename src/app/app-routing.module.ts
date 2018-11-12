c926f36d4a41c8bb97dce315935390fe02260e66
David Arroyo <daarroyo@ucm.es>
2018-11-12 09:14:40 +0100

    Fix imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AsignaturasComponent } from './asignaturas/asignaturas.component';
 import { AsignaturaDetailsComponent } from './asignatura-details/asignatura-details.component';
-import { AnadirEditarAsignaturaComponent } from './anadir-editar-asignatura/anadir-editar-asignatura.component';
 import { AnadirAsignaturaComponent } from './anadir-asignatura/anadir-asignatura.component';
 
 const routes: Routes = [
